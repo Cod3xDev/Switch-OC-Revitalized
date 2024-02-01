9d66d4a143704c74892934c6e4d12870ae85889c
Cod3xDev <67140619+Cod3xDev@users.noreply.github.com>
2024-02-01 16:32:03 +1000

    chore: Update system_settings_parser.py

--- system_settings_parser.py
+++ system_settings_parser.py
@@ -1,46 +1,72 @@
-#!python3
+#!/usr/bin/env python3
 
+# Copyright 2024 Cod3xDev
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
 import os
 import struct
-import sys
 
-try:
-    fpath = sys.argv[1]
-except IndexError:
-    raise SystemExit(f"Usage: {sys.argv[0]} <path>")
+def read_binary_file(file_path):
+    with open(file_path, "rb") as file:
+        file_size = struct.unpack("<I", file.read(4))[0]
+        domain_key_value = parse_file_content(file, file_size)
+    return domain_key_value
 
-file = open(fpath, "rb")
-file.seek(0)
-file_size = int.from_bytes(file.read(4), byteorder="little")
+def parse_file_content(file, file_size):
+    domain_key_value = {}
+    
+    while file.tell() != file_size:
+        domain_key_len = struct.unpack("<I", file.read(4))[0]
+        domain_key = file.read(domain_key_len).decode().rstrip('\x00')
+        domain, key = domain_key.split("!")
 
-domain_key_value = {}
+        domain_key_value.setdefault(domain, {})
 
-while file.tell() != file_size:
-    domain_key_len = int.from_bytes(file.read(4), byteorder="little")
-    domain_key = file.read(domain_key_len).decode().rstrip('\x00')
-    domain, key = domain_key.split("!")
+        val_types = {1: "str", 2: "u8", 3: "u32"}
+        val_type = val_types.get(struct.unpack("<B", file.read(1))[0], "unknown")
+        val_len = struct.unpack("<I", file.read(4))[0]
+        val = file.read(val_len)
 
-    if not domain_key_value.get(domain):
-        domain_key_value[domain] = {}
+        if val_type == "str":
+            value = '"' + val.decode(errors="replace").rstrip('\x00') + '"'
+        else:
+            val_dec = struct.unpack("<I", val)[0]
+            value = f"0x{val_dec:X}"
+            if val_type == "u32" and val_dec >= 10:
+                val_dec_signed = struct.unpack("<i", val)[0]
+                value += f" ; {val_dec_signed}"
 
-    val_types = {1: "str", 2: "u8", 3: "u32"}
-    val_type = val_types.get(int.from_bytes(file.read(1), byteorder="little"), "unknown")
-    val_len = int.from_bytes(file.read(4), byteorder="little")
-    val = file.read(val_len)
+        domain_key_value[domain][key] = f"{val_type}!{value}"
 
-    if val_type == "str":
-        value = '"' + val.decode().rstrip('\x00') + '"'
-    else:
-        val_dec = int.from_bytes(val, byteorder="little")
-        value = "0x" + format(val_dec, 'X')
-        if val_type == "u32" and val_dec >= 10:
-            val_dec = int.from_bytes(val, byteorder="little", signed=True)
-            value += f" ; {val_dec}"
+    return domain_key_value
 
-    domain_key_value[domain][key] = f"{val_type}!{value}"
+def print_parsed_data(domain_key_value):
+    for domain in sorted(domain_key_value.keys()):
+        print(f"[{domain}]")
+        for key in sorted(domain_key_value[domain].keys()):
+            print(f"{key} = {domain_key_value[domain][key]}")
+        print()
 
-for domain in sorted(domain_key_value.keys()):
-    print(f"[{domain}]")
-    for key in sorted(domain_key_value[domain].keys()):
-        print(f"{key} = {domain_key_value[domain][key]}")
-    print()
+def main():
+    parser = argparse.ArgumentParser(description="Parse binary file containing domain, key, and value information.")
+    parser.add_argument("file_path", help="Path to the binary file.")
+    args = parser.parse_args()
+
+    domain_key_value = read_binary_file(args.file_path)
+    print_parsed_data(domain_key_value)
+
+if __name__ == "__main__":
+    main()
